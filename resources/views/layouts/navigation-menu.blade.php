40e0c3aa3b2d23add5b25ef7422dfb9929d03c1f
CodigoArtico <codigoartico@gmail.com>
2026-02-18 12:44:00 -0500

    Menu Indicator According to Route

--- resources/views/layouts/navigation-menu.blade.php
+++ resources/views/layouts/navigation-menu.blade.php
@@ -6,9 +6,13 @@
         </a>
         <ul class="mt-6">
             <li class="relative px-6 py-3">
-                <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                    aria-hidden="true"></span>
-                <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                @if (request()->routeIs('dashboard*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('dashboard*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('dashboard')}}">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                         stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -20,14 +24,26 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('categorias*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('categorias*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('categorias.index')}}">
                     <i class="fa-regular fa-folder-open"></i>
                     <span class="ml-4">Categorias</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('marcas*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('marcas*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('marcas.index')}}">
                     <i class="fa-solid fa-tags"></i>
                     <span class="ml-4">Marcas</span>
@@ -35,7 +51,13 @@
             </li>
 
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('presentaciones*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('presentaciones*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('presentaciones.index')}}">
                     <i class="fa-solid fa-layer-group"></i>
                     <span class="ml-4">Presentaciones</span>
@@ -71,9 +93,13 @@
         </a>
         <ul class="mt-6">
             <li class="relative px-6 py-3">
-                <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                    aria-hidden="true"></span>
-                <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                @if (request()->routeIs('dashboard*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('dashboard*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('dashboard')}}">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                         stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -85,14 +111,26 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('categorias*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('categorias*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('categorias.index')}}">
                     <i class="fa-regular fa-folder-open"></i>
                     <span class="ml-4">Categorias</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('marcas*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('marcas*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('marcas.index')}}">
                     <i class="fa-solid fa-tags"></i>
                     <span class="ml-4">Marcas</span>
@@ -100,7 +138,13 @@
             </li>
 
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('presentaciones*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('presentaciones*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
                     href="{{route('presentaciones.index')}}">
                     <i class="fa-solid fa-layer-group"></i>
                     <span class="ml-4">Presentaciones</span>
